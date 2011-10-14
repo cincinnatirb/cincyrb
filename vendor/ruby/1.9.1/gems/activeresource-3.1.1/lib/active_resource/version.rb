b0b91b92f5e81de0ec07c48cbddc58f46899d9a2
James Smith <st23am@gmail.com>
2011-10-14 12:34:02 -0400

    inital project setup

---
+++ vendor/ruby/1.9.1/gems/activeresource-3.1.1/lib/active_resource/version.rb
@@ -0,0 +1,10 @@
+module ActiveResource
+  module VERSION #:nodoc:
+    MAJOR = 3
+    MINOR = 1
+    TINY  = 1
+    PRE   = nil
+
+    STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
+  end
+end
