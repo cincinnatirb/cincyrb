51bac64098d826c1da3828e3efde8515fe8d38c1
Chris Moore <cdmwebs@gmail.com>
2012-06-15 14:15:02 -0400

    yank the API key from the repo

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -4,7 +4,7 @@ class Meeting
   base_uri 'https://api.meetup.com/2'
 
   def self.get_next_meeting
-    meetings = get("/events?group_id=1460552&fields=name&key=#{MEETUP_API_KEY}")
+    meetings = get("/events?group_id=1460552&fields=name&key=#{ENV.fetch('MEETUP_API_KEY')}")
     meetings["results"].select { |m| m["name"] == "Ruby Brigade" }.first
   end
 end
