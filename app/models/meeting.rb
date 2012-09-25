56f02481ff612f0cec64038e85a67cca84baecdd
Ben Stafford <bendstafford@gmail.com>
2012-09-24 22:06:05 -0400

    Add Meeting.get_upcoming_meetings to return three upcoming meetings, after the next meeting.

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -7,4 +7,9 @@ class Meeting
     meetings = get("/events?group_id=1460552&fields=name&key=#{ENV.fetch('MEETUP_API_KEY')}")
     meetings["results"].select { |m| m["name"] == "Ruby Brigade" }.first
   end
+
+  def self.get_upcoming_meetings
+    meetings = get("/events?group_id=1460552&fields=name&key=#{ENV.fetch('MEETUP_API_KEY')}")
+    meetings["results"].select { |m| m["name"] == "Ruby Brigade" }.slice(1,3)
+  end
 end
