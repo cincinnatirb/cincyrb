c917419057d8ffb87ba354faac44bd2d4f3ca40f
Ben Stafford <bendstafford@gmail.com>
2012-12-01 15:56:28 -0500

    Add Meeting.get_upcoming_meetings and tests, along with VCR cassette for meetup request

---
+++ spec/models/meeting_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe Meeting do
+
+  context "limiting number of results" do
+    before { Meeting.stub!(:ruby_brigade_meetings).and_return([1,2,3,4]) }
+
+    describe ".get_next_meeting" do
+      it "should return the first upcoming event" do
+        Meeting.get_next_meeting.should == 1
+      end
+    end
+
+    describe ".get_upcoming_meetings" do
+      it "should return the next three events that follow the first upcoming event" do
+        Meeting.get_upcoming_meetings.should == [2,3,4]
+      end
+    end
+  end
+
+  describe ".ruby_brigade_meetings" do
+    it "should only return events named as Ruby Brigade" do
+      VCR.use_cassette("meetup") do
+        events = Meeting.ruby_brigade_meetings.map { |meeting| meeting["name"] }
+        events.uniq.should == ["Ruby Brigade"]
+      end
+    end
+  end
+end
---
+++ spec/support/vcr_setup.rb
@@ -0,0 +1,10 @@
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :fakeweb
+end
+
+RSpec.configure do |c|
+  c.extend VCR::RSpec::Macros
+end
