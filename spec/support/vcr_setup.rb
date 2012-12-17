8ca2fee214478aee43050b01369f3ae6d99925e6
Ben Stafford <bendstafford@gmail.com>
2012-12-17 13:45:21 -0500

    Prevent API key from appearing in cassette if it's recorded again

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -3,6 +3,7 @@ require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :fakeweb
+  c.filter_sensitive_data('<API KEY>') { ENV.fetch('MEETUP_API_KEY') }
 end
 
 RSpec.configure do |c|
