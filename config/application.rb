0322bac95b38e014191310daae094df19d6588b1
James Smith <st23am@gmail.com>
2013-01-10 11:38:57 -0500

    asset precompile fix for heroku

--- config/application.rb
+++ config/application.rb
@@ -51,5 +51,7 @@ module Cincirb
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.initialize_on_precompile = false
   end
 end
