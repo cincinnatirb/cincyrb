9665b509e04359d78ca11cdd2f62ab761f67b589
Ryan Arthur <ryan.arthur5819@gmail.com>
2013-05-30 14:06:38 -0400

    use gravatars on past events

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,14 @@
+require 'digest/md5'
 module ApplicationHelper
 
   def format_date_for_homepage(date)
     Time.at(date/1000).to_date.strftime("%b") +
       "<span>" + Time.at(date/1000).to_date.strftime("%e") + "</span>"
   end
+
+  def gravatar_url(email_address)
+    hash = Digest::MD5.hexdigest(email_address.to_s)
+    image_src = "http://www.gravatar.com/avatar/#{hash}?s=127"
+  end
 end
+
