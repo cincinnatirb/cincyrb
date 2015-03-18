30e1955542bcedc09c5f13fa92abe8ddb26964f1
Cliff Campbell <cliff.campbell@crakn.net>
2015-03-18 11:48:11 -0400

    fixing tests for Twitter to use VCR

--- spec/controllers/twitter_controller_spec.rb
+++ spec/controllers/twitter_controller_spec.rb
@@ -1,11 +1,19 @@
 require 'spec_helper'
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/cassettes"
+  config.hook_into :fakeweb
+end
 
 describe TwitterController do
 
   describe "GET 'timeline'" do
     it "returns http success" do
-      get 'timeline'
-      response.should be_success
+      VCR.use_cassette("twitter", :match_requests_on => [:host, :path]) do
+        get 'timeline', :format => :json
+        response.should be_success
+      end
     end
   end
 
