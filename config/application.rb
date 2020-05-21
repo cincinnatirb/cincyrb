ef9465ebc937d920eb3d794b31d422d95fa1d279
Bill Barnett <bill@agilo.us>
2020-05-21 16:37:25 -0400

    Rails upgrade from 4.1 to 4.2 * includes Ruby upgrade from 2.3.5 to 2.6.5 * removed haml-rails gem (WIP) * pinned activeadmin gem to 1.4.3 due to later   versions depending on Rails 5 * pinned rspec-rails gem to ~> 4.0 due to later   versions depending on Rails 5

--- config/application.rb
+++ config/application.rb
@@ -41,6 +41,9 @@ module Cincirb
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
