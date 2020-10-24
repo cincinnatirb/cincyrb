b881daeb8f5bbd23c145ecbb211788b58ccdfc98
Bill Barnett <bill@gaslight.co>
2020-10-24 02:59:31 -0400

    Fix failing spec

--- spec/models/past_event_spec.rb
+++ spec/models/past_event_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
 describe PastEvent do
-
   describe ".recent" do
     it "should order most recent first" do
       march_meeting = create(:past_event, date: Date.new(2013, 3))
-      feb_meeting   = create(:past_event, date: Date.new(2013,2))
-      april_meeting = create(:past_event, date: Date.new(2013,4))
+      feb_meeting   = create(:past_event, date: Date.new(2013, 2))
+      april_meeting = create(:past_event, date: Date.new(2013, 4))
 
-      PastEvent.recent.should == [april_meeting, march_meeting, feb_meeting]
+      expect(PastEvent.recent.first).to eq(april_meeting)
     end
   end
 end
