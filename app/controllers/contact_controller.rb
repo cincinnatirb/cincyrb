160fe21af4660a07179aef978ea0148403c0ab18
Bill Barnett <bill@gaslight.co>
2020-10-24 00:17:32 -0400

    Use strong params

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,6 +1,6 @@
 class ContactController < ApplicationController
   def create
-    @contact = Contact.new(params[:contact])
+    @contact = Contact.new(contact_params)
     if @contact.invalid?
       messages = @contact.errors.full_messages.to_sentence
       render json: { errors: messages }, status: :unprocessable_entity
@@ -8,5 +8,11 @@ class ContactController < ApplicationController
       ContactMailer.contact_email(@contact).deliver
       render json: { message: 'Your feedback has been submitted. We will reply as soon as possible.' }
     end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:email, :message, :name, :subject)
   end
 end
