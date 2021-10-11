e7893512dc3b9769ca9d6cef59f4aa94a6ddfc9b
chtelmko <katerynachtel@gmail.com>
2021-10-11 09:22:54 -0400

    upgrate rails to 6.0
    co-authored-by: Bill Barnett <wm.barnett@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -1,76 +1,76 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130530180251) do
+ActiveRecord::Schema.define(version: 2021_10_11_130706) do
 
-  create_table "active_admin_comments", :force => true do |t|
-    t.string   "resource_id",   :null => false
-    t.string   "resource_type", :null => false
-    t.integer  "author_id"
-    t.string   "author_type"
-    t.text     "body"
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string "resource_id", null: false
+    t.string "resource_type", null: false
+    t.integer "author_id"
+    t.string "author_type"
+    t.text "body"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "namespace"
+    t.string "namespace"
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_admin_notes_on_resource_type_and_resource_id"
   end
 
-  add_index "active_admin_comments", ["author_type", "author_id"], :name => "index_active_admin_comments_on_author_type_and_author_id"
-  add_index "active_admin_comments", ["namespace"], :name => "index_active_admin_comments_on_namespace"
-  add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
-
-  create_table "admin_users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
-    t.string   "reset_password_token"
+  create_table "admin_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer "sign_in_count", default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
-  add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
-  add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
-
-  create_table "past_events", :force => true do |t|
-    t.string   "topic"
+  create_table "past_events", force: :cascade do |t|
+    t.string "topic"
     t.datetime "date"
-    t.text     "description"
+    t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "past_events_speakers", :id => false, :force => true do |t|
+  create_table "past_events_speakers", id: false, force: :cascade do |t|
     t.integer "speaker_id"
     t.integer "past_event_id"
   end
 
-  create_table "speakers", :force => true do |t|
-    t.string   "name"
-    t.text     "bio"
+  create_table "speakers", force: :cascade do |t|
+    t.string "name"
+    t.text "bio"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "image_url"
-    t.string   "email"
+    t.string "image_url"
+    t.string "email"
   end
 
-  create_table "videos", :force => true do |t|
-    t.string   "url"
-    t.integer  "past_event_id"
+  create_table "videos", force: :cascade do |t|
+    t.string "url"
+    t.integer "past_event_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
