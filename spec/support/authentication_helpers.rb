43059e8e42f0860a627737f6e452dc03ef6911a9
Bill Barnett <bill@gaslight.co>
2021-10-29 19:55:18 -0400

    Add authentication helpers

---
+++ spec/support/authentication_helpers.rb
@@ -0,0 +1,22 @@
+module SystemAuthenticationHelpers
+  def sign_in_as(email, password)
+    visit new_admin_user_session_path
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password
+    click_button 'Login'
+  end
+end
+
+module RequestAuthenticationHelpers
+  include Warden::Test::Helpers
+
+  def sign_in_as(admin_user)
+    scope = Devise::Mapping.find_scope!(admin_user)
+    login_as(admin_user, scope: scope)
+  end
+
+  def sign_out(admin_user)
+    scope = Devise::Mapping.find_scope!(admin_user)
+    logout(scope)
+  end
+end
