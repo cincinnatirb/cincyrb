3ebd76783a1c580771e026421fe3dec3b597e4fe
Bill Barnett <bill@gaslight.co>
2021-11-05 10:06:47 -0400

    whitespace

--- spec/system/past_events_spec.rb
+++ spec/system/past_events_spec.rb
@@ -14,9 +14,11 @@ RSpec.describe "Past Events", type: :system do
     end
 
     it { is_expected.to have_text(past_event.topic) }
+
     it 'lists all event speakers' do
       expect(page.all('div.speaker').count).to eq(past_event.speakers.count)
     end
+
     it { is_expected.to have_text(past_event.speakers.first.name) }
     it { is_expected.to have_text(past_event.speakers.last.name) }
     it { is_expected.to have_selector('div.past-event__video') }
