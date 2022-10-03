3affc080e76ea37178dabe6e997e956a73dcf4a2
Bill Barnett <bill.barnett@hey.com>
2022-10-03 09:15:09 -0400

    upgrade to Ruby 3.0

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-ruby '2.7.2'
+ruby '3.0.4'
 
 gem 'activeadmin'
 gem 'bootsnap'
--- db/schema.rb
+++ db/schema.rb
@@ -10,44 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_30_114808) do
+ActiveRecord::Schema.define(version: 2021_11_07_134636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string "resource_id", null: false
-    t.string "resource_type", null: false
-    t.integer "author_id"
-    t.string "author_type"
-    t.text "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string "namespace"
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_admin_notes_on_resource_type_and_resource_id"
-  end
-
-  create_table "admin_users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
+  create_table "admin_users", id: :serial, force: :cascade do |t|
+    t.string "email", limit: 255, default: "", null: false
+    t.string "encrypted_password", limit: 255, default: "", null: false
+    t.string "reset_password_token", limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer "sign_in_count", default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
+    t.string "current_sign_in_ip", limit: 255
+    t.string "last_sign_in_ip", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
-  create_table "past_events", force: :cascade do |t|
-    t.string "topic"
+  create_table "past_events", id: :serial, force: :cascade do |t|
+    t.string "topic", limit: 255
     t.datetime "date"
     t.text "description"
     t.datetime "created_at"
@@ -60,17 +46,17 @@ ActiveRecord::Schema.define(version: 2021_10_30_114808) do
     t.index ["past_event_id", "speaker_id"], name: "index_speaker_assignments_on_past_event_id_and_speaker_id"
   end
 
-  create_table "speakers", force: :cascade do |t|
-    t.string "name"
+  create_table "speakers", id: :serial, force: :cascade do |t|
+    t.string "name", limit: 255
     t.text "bio"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string "image_url"
-    t.string "email"
+    t.string "image_url", limit: 255
+    t.string "email", limit: 255
   end
 
-  create_table "videos", force: :cascade do |t|
-    t.string "url"
+  create_table "videos", id: :serial, force: :cascade do |t|
+    t.string "url", limit: 255
     t.integer "past_event_id"
     t.datetime "created_at"
     t.datetime "updated_at"
