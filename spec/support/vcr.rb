623b4c0a51cceab983c562f70e7c003f5a85d662
Bill Barnett <bill.barnett@hey.com>
2025-03-21 10:50:38 -0400

    fix VCR issue in GitHub Action

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -6,4 +6,18 @@ VCR.configure do |c|
 
   c.ignore_localhost = true
   c.ignore_hosts 'chromedriver.storage.googleapis.com'
+
+  # Configure VCR to allow connections when no cassette is present in CI
+  # This will allow the tests to run in GitHub Actions without Bluesky credentials
+  c.allow_http_connections_when_no_cassette = true if ENV['CI'] == 'true'
+
+  # Configure VCR to ignore Bluesky API requests
+  c.ignore_request do |request|
+    URI(request.uri).host == 'bsky.social'
+  end
+
+  # Configure VCR to use the correct record mode in CI
+  c.default_cassette_options = {
+    record: ENV['VCR_RECORD_MODE']&.to_sym || :once
+  }
 end
