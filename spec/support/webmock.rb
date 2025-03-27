6763ea017d3667ac306ef9b3f2d3d1b3754f4fc5
Bill Barnett <bill.barnett@hey.com>
2025-03-27 11:11:27 -0400

    use `RAILS_MASTER_KEY`

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -4,9 +4,19 @@ WebMock.disable_net_connect!(allow_localhost: true)
 
 RSpec.configure do |config|
   config.before do
+    stub_request(:get, "https://bsky.social/xrpc/com.atproto.identity.resolveHandle?handle=cincyrb.com")
+      .with(
+        headers: {
+          'Accept' => '*/*',
+          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent' => 'Ruby'
+        }
+      )
+      .to_return(status: 200, body: "", headers: {})
+
     stub_request(:post, "https://bsky.social/xrpc/com.atproto.server.createSession")
       .with(
-        body: { identifier: "", password: "" }.to_json,
+        body: "{\"identifier\":\"#{Rails.application.credentials.dig(:bluesky, :username)}\",\"password\":\"#{Rails.application.credentials.dig(:bluesky, :app_password)}\"}",
         headers: {
           'Accept' => '*/*',
           'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
@@ -14,6 +24,6 @@ RSpec.configure do |config|
           'User-Agent' => 'Ruby'
         }
       )
-      .to_return(status: 200, body: { accessJwt: "fake-jwt-token" }.to_json, headers: {})
+      .to_return(status: 200, body: "", headers: {})
   end
 end
