7858ded620a9493cc7914934459305e361ed9e8d
Bill Barnett <bill.barnett@hey.com>
2025-03-27 10:34:42 -0400

    rubocop & nil changed to ""

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -3,17 +3,17 @@ require "webmock/rspec"
 WebMock.disable_net_connect!(allow_localhost: true)
 
 RSpec.configure do |config|
-  config.before(:each) do
+  config.before do
     stub_request(:post, "https://bsky.social/xrpc/com.atproto.server.createSession")
       .with(
-        body: {identifier: nil, password: nil}.to_json,
+        body: { identifier: "", password: "" }.to_json,
         headers: {
-          'Accept'=>'*/*',
-          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-          'Content-Type'=>'application/json',
-          'User-Agent'=>'Ruby'
+          'Accept' => '*/*',
+          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'Content-Type' => 'application/json',
+          'User-Agent' => 'Ruby'
         }
       )
-      .to_return(status: 200, body: {accessJwt: "fake-jwt-token"}.to_json, headers: {})
+      .to_return(status: 200, body: { accessJwt: "fake-jwt-token" }.to_json, headers: {})
   end
 end
