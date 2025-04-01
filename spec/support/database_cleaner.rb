0b3926be25ac755fc20c39d9da38cd76bd64e8a9
Bill Barnett <bill.barnett@hey.com>
2025-04-01 13:45:29 -0400

    change DB cleaning strategy for multi-treaded system specs

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,12 +1,14 @@
 RSpec.configure do |config|
   config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
   end
 
-  config.around do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
+  config.before do |example|
+    DatabaseCleaner.strategy = example.metadata[:type] == :system ? :truncation : :transaction
+    DatabaseCleaner.start
+  end
+
+  config.after do
+    DatabaseCleaner.clean
   end
 end
